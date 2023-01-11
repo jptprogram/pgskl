2eec420163debcc4daa4b9550f97c6b1332bb0aa
JuanPedro Toro <jpdungeon@gmail.com>
2023-01-10 21:50:46 -0300

    dia 1

--- app/controllers/equipos_controller.rb
+++ app/controllers/equipos_controller.rb
@@ -1,2 +1,25 @@
 class EquiposController < ApplicationController
+
+  def index
+    @equipos = Equipo.all
+  end
+
+  def show
+    @equipo = Equipo.find(params[:id])
+  end
+
+  def new
+    @equipo = Equipo.new
+  end
+
+  def create
+    @equipo = Equipo.create(equipo_params)
+    redirect_to equipo_path(@equipo)
+  end
+
+  private
+
+  def equipo_params
+    params.require(:equipo).permit(:modelo,:marca,:disponible,:link,:photo)
+  end
 end
--- app/models/equipo.rb
+++ app/models/equipo.rb
@@ -1,2 +1,4 @@
 class Equipo < ApplicationRecord
+
+  has_one_attached :photo
 end
