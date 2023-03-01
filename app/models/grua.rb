5bce883ac7be4c410780200e1bf92915ee7a7e01
Daniel Enriquez <daniel.enm17@gmail.com>
2023-03-01 10:20:11 -0600

    fix table remove fixed types

--- app/models/grua.rb
+++ app/models/grua.rb
@@ -1,3 +1,7 @@
 class Grua < ApplicationRecord
   has_one_attached :photo
 end
+# t.boolean "electrica"
+# t.boolean "combustion"
+# t.boolean "apiladores"
+# t.boolean "transpaleta"
--- db/schema.rb
+++ db/schema.rb
@@ -58,10 +58,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_154820) do
     t.string "disponible"
     t.string "link"
     t.string "photo"
-    t.boolean "electrica"
-    t.boolean "combustion"
-    t.boolean "apiladores"
-    t.boolean "transpaleta"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
