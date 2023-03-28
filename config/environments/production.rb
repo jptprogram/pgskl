cb3bfbd590fe7a2aed887b3935b854a6d8624dd9
Daniel Enriquez <daniel.enm17@gmail.com>
2023-03-28 11:05:10 -0600

    add envs and gemfile configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,20 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  host = "https://pgskl.herokuapp.com"
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {address:              "smtp.sendgrid.net",
+                                        port:                 587,
+                                        user_name:            ENV["SENDGRID_USERNAME"],
+                                        password:             ENV["SENDGRID_PASSWORD"],
+                                        authentication:       "plain",
+                                        enable_starttls_auto: true }
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
