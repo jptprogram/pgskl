f051d43755ccc4cbfb1cc497bd89906cc5507a6a
Daniel Enriquez <daniel.enm17@gmail.com>
2023-03-31 15:44:18 -0600

    pages index showing correctly

---
+++ app/controllers/transpaleta_apilador_electricos_controller.rb
@@ -0,0 +1,32 @@
+class TranspaletaApiladorElectricosController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  def index
+    @traspaletas = TranspaletaApiladorElectrico.all
+  end
+
+  def show
+    @traspaleta = TranspaletaApiladorElectrico.find(params[:id])
+  end
+
+  def new
+    @traspaleta = TranspaletaApiladorElectrico.new
+  end
+
+  def create
+    @traspaleta = TranspaletaApiladorElectrico.create(traspaleta_params)
+    redirect_to transpaleta_apilador_electricos_path(@traspaleta)
+  end
+
+  def destroy
+    @traspaleta = TranspaletaApiladorElectrico.find(params[:id])
+    @traspaleta.destroy
+
+    redirect_to transpaleta_apilador_electricos_path
+  end
+
+  private
+
+  def traspaleta_params
+    params.require(:traspaleta).permit(:modelo, :marca, :disponible, :link, :photo, :descripcion)
+  end
+end
