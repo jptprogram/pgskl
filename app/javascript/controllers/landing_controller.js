864ab1552c179b0c9d61a2159ceec0b5b1a60711
Daniel Enriquez <daniel.enm17@gmail.com>
2023-03-31 14:30:13 -0600

    landing finished, missing navbar redirects

--- app/javascript/controllers/landing_controller.js
+++ app/javascript/controllers/landing_controller.js
@@ -2,7 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="landing"
 export default class extends Controller {
+
+  static targets = ["emailFieldUpper", "emailFieldLower", "firstNameField"];
+
   connect() {
-    console.log("Landing controller connected");
+  }
+
+  scrollToContact(event) {
+    event.preventDefault();
+    const contact = document.getElementById("contact");
+    contact.scrollIntoView({ behavior: "smooth" });
+    const upperEmail = this.emailFieldUpperTarget.value;
+    this.emailFieldLowerTarget.value = upperEmail;
+    this.firstNameFieldTarget.focus();
   }
 }
