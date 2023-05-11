1e531ff309677dea09fa204880d05c0b6bfdeb06
Daniel Enriquez <daniel.enm17@gmail.com>
2023-05-10 18:05:19 -0600

    changed tests

--- db/migrate/20230331210759_change_disponible_column_type.rb
+++ db/migrate/20230331210759_change_disponible_column_type.rb
@@ -1,11 +1,11 @@
 class ChangeDisponibleColumnType < ActiveRecord::Migration[7.0]
   def up
-    execute "UPDATE equipos SET disponible = 't' WHERE disponible = 'INMEDIATA'"
-    execute "UPDATE gruas SET disponible = 't' WHERE disponible = 'INMEDIATA'"
-    execute "UPDATE maquinariapesadas SET disponible = 't' WHERE disponible = 'INMEDIATA'"
-    execute "UPDATE repuestos SET disponible = 't' WHERE disponible = 'INMEDIATA'"
-    execute "UPDATE tractors SET disponible = 't' WHERE disponible = 'INMEDIATA'"
-    execute "UPDATE transpaleta_apilador_electricos SET disponible = 't' WHERE disponible = 'INMEDIATA'"
+    execute "UPDATE equipos SET disponible = 't' WHERE disponible = 'INMEDIATA '"
+    execute "UPDATE gruas SET disponible = 't' WHERE disponible = 'INMEDIATA '"
+    execute "UPDATE maquinariapesadas SET disponible = 't' WHERE disponible = 'INMEDIATA '"
+    execute "UPDATE repuestos SET disponible = 't' WHERE disponible = 'INMEDIATA '"
+    execute "UPDATE tractors SET disponible = 't' WHERE disponible = 'INMEDIATA '"
+    execute "UPDATE transpaleta_apilador_electricos SET disponible = 't' WHERE disponible = 'INMEDIATA '"
 
 
     change_column :equipos, :disponible, 'boolean USING CAST(disponible AS boolean)'
